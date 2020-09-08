a27d03cecc710a167213e05f2dcb5bf394d77515
Thibault Chataigner <t.chataigner@criteo.com>
2020-09-08 14:04:40 +0200

    Build script: add yaml file with built version
    Signed-off-by: Thibault Chataigner <t.chataigner@criteo.com>

--- scripts/ci/build.sh
+++ scripts/ci/build.sh
@@ -12,10 +12,14 @@ mkdir -p dist/
 for d in ${DIST}; do
 
     # Build required values
+    echo "version: ${VERSION}" > ${d}/nse/FEED_INFO.yaml
     PACKAGE=nse-${d}-feed-${VERSION}.tar.gz
 
     # Packaging
     tar -czf dist/${PACKAGE} -C ${d}/nse --transform "s,.,./nse/${d}," .
+
+    # Cleanup
+    rm ${d}/nse/FEED_INFO.yaml
 
 done
 
